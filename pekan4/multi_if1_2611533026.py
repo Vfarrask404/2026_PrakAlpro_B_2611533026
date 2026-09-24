d96b69163f44027dc3a6f55208ac436d469b2c5e
Vfarrask404 <vaniafarraskalista@gmail.com>
2026-09-24 10:00:47 +0700

    pekan 4 don

--- pekan4/multi_if1_2611533026.py
+++ pekan4/multi_if1_2611533026.py
@@ -4,11 +4,11 @@ sim_3026 = input("Apakah anda udah punya SIM C (y/t): ") [0]
 if umur_3026 >= 17 and sim_3026 == 'y' :
     print("anda sudah dewasa dan boleh bawa motor")
 
-    if umur_3026 >= 17 and sim_3026 != 'y' :
-        print("anda sudah dewasa tetapi tidak boleh bawa motor")
+if umur_3026 >= 17 and sim_3026 != 'y' :
+    print("anda sudah dewasa tetapi tidak boleh bawa motor")
 
-        if umur_3026 < 17 and sim_3026 == 'y' :
-            print("anda belum cukup umur punya SIM")
+if umur_3026 < 17 and sim_3026 == 'y' :
+    print("anda belum cukup umur punya SIM")
 
-            if umur_3026 < 17 and sim_3026 != 'y' :
-                print ("anda belum cukup umur bawa motor")
+if umur_3026 < 17 and sim_3026 != 'y' :
+    print ("anda belum cukup umur bawa motor")
--- pekan4/multi_if2_2611533026.py
+++ pekan4/multi_if2_2611533026.py
@@ -16,11 +16,11 @@ total_diskon_persen_3026 = 0
 if total_belanja_3026 > 1000000 :
     total_diskon_persen_3026 += 10 # diskon belanja besar
 
-    if is_member_3026 :
-        total_diskon_persen_3026 += 5 #diskon member
+if is_member_3026 :
+    total_diskon_persen_3026 += 5 #diskon member
 
-        if kode_promo_valid_3026 :
-            total_diskon_persen_3026 += 15 #diskon voucher
+if kode_promo_valid_3026 :
+    total_diskon_persen_3026 += 15 #diskon voucher
 
 #menghitung nominal diskon dan total bayar
 nominal_diskon_3026 = total_belanja_3026 * (total_diskon_persen_3026 / 100)
@@ -28,8 +28,8 @@ total_bayar_3026 = total_belanja_3026 - nominal_diskon_3026
 
 #output hasil
 print("\n---Rincian Pembayaran---")
-print(f"total diskon :  {total_diskon_persen_3026}% (Rp {nominal_diskon_3026 :,0f})")
-print(f"Total Bayar : Rp {total_bayar_3026:,0f}")
+print(f"total diskon :  {total_diskon_persen_3026}% (Rp {nominal_diskon_3026 :,.0f})")
+print(f"Total Bayar : Rp {total_bayar_3026:,.0f}")
 
 print(f"Total diskon yang anda dapatkan :  {total_diskon_persen_3026}%")
 #output :total diskon yang anda dapatkan : 30% jika belanja > 1 juta, member, dan kode promo valid
